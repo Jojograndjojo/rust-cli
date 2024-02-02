20ee592d5dbe8a01856182bd1be35cdf5b88cc4a
Jonathan Passé <jpasse@thoughtworks.com>
2024-02-02 13:45:23 +0100

    refactor: rename second_level_method variable

--- src/second_level.rs
+++ src/second_level.rs
@@ -8,8 +8,8 @@ pub trait SecondLevelTrait {
 pub struct SecondLevel {}
 
 impl SecondLevelTrait for SecondLevel {
-    fn second_level_method(&self, _second_level_flag: String) -> Result<(), anyhow::Error> {
-        println!("Here is the second flag value: {}", _second_level_flag);
+    fn second_level_method(&self, second_level_flag: String) -> Result<(), anyhow::Error> {
+        println!("Here is the second flag value: {}", second_level_flag);
         Ok(())
     }
 }
